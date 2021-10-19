f9b7c8659561e5eb2bbba06c0078084c9166b380
Ariful Alam <alamariful1727@gmail.com>
2021-10-20 02:33:08 +0600

    Ant's Search component added with some navbar redesign.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,13 +4,13 @@ import { NavLink } from 'react-router-dom';
 const Footer = () => {
 	return (
 		<div className='flex justify-center items-center border-t px-6 py-3'>
-			<p className='text-sm font-semibold'>
+			<p className='text-sm font-semibold text-center'>
 				© Restaurant Finder {new Date().getFullYear()}. Developed by{' '}
 				<NavLink
 					to={{ pathname: 'https://www.linkedin.com/in/alamariful1727' }}
 					target='_blank'
 					rel='noopener noreferrer'
-					className='text-blue-600'>
+					className='text-blue-600 hover:text-blue-400'>
 					Ariful Alam
 				</NavLink>
 			</p>
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css';
 import './styles/index.css';
 
 ReactDOM.render(
