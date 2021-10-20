94b6b3a33eafd15b1dda3e9947756c16ea3a4cff
Ariful Alam <alamariful1727@gmail.com>
2021-10-20 13:13:12 +0600

    require check added for env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,20 @@
 import axios from 'axios';
 
+if (!process.env.REACT_APP_FSQ_CLIENT_ID) {
+	throw new Error('REACT_APP_FSQ_CLIENT_ID is required');
+} else if (!process.env.REACT_APP_FSQ_CLIENT_SECRET) {
+	throw new Error('REACT_APP_FSQ_CLIENT_SECRET is required');
+} else if (!process.env.REACT_APP_FSQ_VERSION) {
+	throw new Error('REACT_APP_FSQ_VERSION is required');
+} else if (!process.env.REACT_APP_GOOGLE_API_KEY) {
+	throw new Error('REACT_APP_GOOGLE_API_KEY is required');
+}
+
 export const API_ENDPOINT_LAMBDA = 'https://api.foursquare.com/v2';
 export const FSQ_CLIENT_ID = process.env.REACT_APP_FSQ_CLIENT_ID;
 export const FSQ_CLIENT_SECRET = process.env.REACT_APP_FSQ_CLIENT_SECRET;
 export const FSQ_VERSION = process.env.REACT_APP_FSQ_VERSION;
+export const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 export const FSQApi = axios.create({
 	baseURL: API_ENDPOINT_LAMBDA,
